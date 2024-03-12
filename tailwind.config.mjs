2daec66c8e425d21b7cf608514e1eebfbe55b341
Essential Randomness <essential.randomn3ss@gmail.com>
2024-03-11 18:35:37 -0700

    made a browser window

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,16 +2,21 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    colors: {
-      "lemon-dark": "#ffbf00",
-      "lemon-dark-transparent": "#ffbf0080",
-      lemon: "#f6d880",
-    },
     fontFamily: {
       hatch: ['"Hatch"', "sans-serif"],
+      zeitung: ['"Zeitung Mono Pro"', "sans-serif"],
       // Add more custom font families as needed
     },
-    extend: {},
+    extend: {
+      colors: {
+        "lemon-dark": "#ffbf00",
+        "lemon-dark-transparent": "#ffbf0080",
+        lemon: "#f6d880",
+        "teal-dark": "#5a99dc",
+        teal: "#5be2ef",
+        "teal-light": "#c8f4f4",
+      },
+    },
   },
   plugins: [],
 };

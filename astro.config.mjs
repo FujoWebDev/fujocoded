43bbcbd456ac5ebd2ef3d0ee171de78518e6376f
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2024-03-25 11:03:39 -0700

    Adds Updates collection and pages (#3)
    Co-authored-by: Ms. Boba <essentialrandomn3ss@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-
+import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind()]
+  integrations: [icon(), tailwind(), mdx()],
 });

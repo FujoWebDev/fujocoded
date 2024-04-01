23d02f3e547dee2c5c9d769ce1507885b4834bf3
enigmalea <enigmalea@gmail.com>
2024-04-01 04:33:25 -0400

    added astro dev toolbar metadata viewer.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
+import metaTags from "astro-meta-tags";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind(), mdx()],
+  integrations: [icon(), tailwind(), mdx(), metaTags()]
 });

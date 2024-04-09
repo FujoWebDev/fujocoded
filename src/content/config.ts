996341c00a4bbe7ccac8195d92e6bb0585dbd84a
Essential Randomness <essential.randomn3ss@gmail.com>
2024-04-08 18:05:52 -0700

    create content collection for sponsors

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,6 +2,7 @@ import { collection as teamCollection } from "./team/_config.ts";
 import { collection as fujinCollection } from "./fujin/_config.ts";
 import { collection as projectsCollection } from "./projects/_config.ts";
 import { collection as imagesCollection } from "./images/_config.ts";
+import { collection as sponsorsCollection } from "./sponsors/_config.ts";
 import { updates } from "./updates/_config.ts";
 
 export const collections = {
@@ -10,4 +11,5 @@ export const collections = {
   fujin: fujinCollection,
   projects: projectsCollection,
   images: imagesCollection,
+  sponsors: sponsorsCollection,
 };
---
+++ src/content/sponsors/_config.ts
@@ -0,0 +1,26 @@
+import { defineCollection, z } from "astro:content";
+import { socialsSchema, transformSocial } from "../../lib/socials-transformer";
+
+const Pledge = z.union([
+  z.object({
+    campaign: z.enum(["FujoGuide Volume 0"]),
+    tier: z.enum(["GIT Art", "Sponsor"]),
+  }),
+  z.object({
+    campaign: z.enum(["FujoCoded LLC"]),
+    tier: z.enum(["Bronze", "Silver", "Gold"]),
+  }),
+]);
+
+export const collection = defineCollection({
+  type: "data",
+  schema: (tools) =>
+    z.object({
+      name: z.string(),
+      avatar: tools.image(),
+      pledges: Pledge.array(),
+      contacts: socialsSchema
+        .array()
+        .transform((contacts) => contacts.map(transformSocial)),
+    }),
+});

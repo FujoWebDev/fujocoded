ac103526c6a0981fc328b1d573c0e7474f970d0a
Essential Randomness <essential.randomn3ss@gmail.com>
2024-05-16 16:11:32 -0700

    get the konami code to display at the bottom of the page

--- src/components/KonamiCode.ts
+++ src/components/KonamiCode.ts
@@ -14,27 +14,43 @@ class KonamiCode extends HTMLElement {
     "KeyA",
   ];
   nextIndex = 0;
+  buttonElements: Element[] = [];
 
   constructor() {
     super();
   }
 
   connectedCallback() {
-    console.log("The Konami Code has been added to the page");
+    const buttonTemplate =
+      this.querySelector<HTMLTemplateElement>(".button-template");
+    if (!buttonTemplate) {
+      throw new Error(
+        "You must provide a template for the Konami Code buttons",
+      );
+    }
+    KonamiCode.KONAMI_SEQUENCE.forEach((key) => {
+      const newButton = (buttonTemplate.content.cloneNode(true) as Element)
+        .firstElementChild!;
+      newButton.classList.add(key.toLowerCase());
+      this.buttonElements.push(this.appendChild(newButton));
+    });
+
     document.addEventListener("keydown", (event) => {
       if (KonamiCode.KONAMI_SEQUENCE[this.nextIndex] == event.code) {
-        console.log("Way to go")!;
         this.nextIndex = this.nextIndex + 1;
 
         if (this.nextIndex == KonamiCode.KONAMI_SEQUENCE.length) {
-          console.log("Congratulations");
           this.nextIndex = 0;
+          this.classList.add("triggered");
           document.dispatchEvent(new Event(KonamiTriggeredEventCode));
         }
       } else {
-        console.log("try again");
         this.nextIndex = 0;
       }
+
+      this.buttonElements.forEach((element, index) => {
+        element.classList.toggle("hit", index < this.nextIndex);
+      });
     });
   }
 }

872c8b090f6f3d9805f3dae991f8e519f3d21834
Essential Randomness <essentialrandomn3ss@gmail.com>
2025-12-23 00:22:25 -0800

    add haetae to site

--- src/content/contributors/_schema/projects.ts
+++ src/content/contributors/_schema/projects.ts
@@ -9,6 +9,7 @@ export const PROJECTS = [
   "Fandom Cookies",
   "FujoCoded",
   "Learn@",
+  "Code Contributor",
 ] as const;
 export type Project = (typeof PROJECTS)[number];
 
@@ -47,6 +48,7 @@ export const FANDOM_COOKIES_ROLES = [
 ];
 
 export const LEARN_AT_ROLES = ["Writers Coordinator", "Article Writer"];
+export const CODE_CONTRIBUTOR_ROLES = ["Documentation"];
 
 export const PROJECT_ROLES: Record<Project, string[]> = {
   "Volume 0 Issue 1": VOLUME_0_ISSUE_1_ROLES,
@@ -56,6 +58,7 @@ export const PROJECT_ROLES: Record<Project, string[]> = {
   "Fandom Cookies": FANDOM_COOKIES_ROLES,
   FujoCoded: FUJOCODED_ROLES,
   "Learn@": LEARN_AT_ROLES,
+  "Code Contributor": CODE_CONTRIBUTOR_ROLES,
 };
 
 export default PROJECTS;

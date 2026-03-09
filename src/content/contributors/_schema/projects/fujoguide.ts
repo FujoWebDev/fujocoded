b8c732ac4bde912ff54b21bfdd9e132d5fc72573
Essential Randomness <essentialrandomn3ss@gmail.com>
2026-03-08 23:56:38 -0700

    New contributors

---
+++ src/content/contributors/_schema/projects/fujoguide.ts
@@ -0,0 +1,50 @@
+export const VOLUME_0_KICKSTARTER_ROLES = [
+  "Additional Art & Layout",
+  "Additional Research, Feedback, Development, and Assistance",
+  "Art Director",
+  "Artist",
+  "Character Designer",
+  "Editor",
+  "Graphic Design",
+  "Lead Research",
+  "Merch Designer",
+  "Project Lead",
+  "Project Organizing",
+  "QA Testing",
+  "Sensitivity Reading",
+  "Social Media & Marketing",
+  "Technical Writer",
+  "Writer",
+];
+
+export const VOLUME_0_ROLES = [
+  "Additional Coding",
+  "Artist",
+  "Beta Reader",
+  "Beta Reading Coordinator",
+  "Beta Wrangler",
+  "Character Designer",
+  "Editor",
+  "Project Lead",
+  "Project Organizing",
+  "Technical Writer",
+  "The One Who Talks With The Lawyers",
+  "Writer",
+];
+
+export const VOLUME_0_ISSUE_1_ROLES = [
+  "Additional Coding",
+  "Artist",
+  "Beta Reader",
+  "Beta Reading Coordinator",
+  "Character Designer",
+  "Communications",
+  "Data Collection & Entry",
+  "Project Management",
+  "Proofreader",
+  "Scenario Writer",
+  "Tasks Coordinator",
+  "Technical Writer",
+];
+
+export const WEBSITES_ROLES = ["fujoweb.dev", "fujocoded.com"];
